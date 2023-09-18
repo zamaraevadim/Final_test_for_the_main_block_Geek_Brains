db7bf8b686f3fadad4e03c0bdbe4fc21637b321f
Пользователь <zamaraevadim@yandex.ru>
2023-09-18 22:21:39 +0500

    Создал цикл для заполнения нового массива

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        String[] arr  = new String[] {"Hello", "2", "world", ":-)"};
 
+        int count = 0;
+        for(int i = 0; i < arr.length;i++){
+            if(arr[i].length() <= 3){
+                count++;
+            }
+        }
+        String[] arr2 = new String[count];
+        int count1 = 0;
+        for(int i = 0; i < arr.length;i++){
+            if(arr[i].length() <= 3){
+                arr2[count1] = arr[i];
+                count1++;
+            }
+        }
     }
 }
